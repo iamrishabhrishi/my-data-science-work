34a57dc8b95e8d6e00791f84284bba0c84d1b642
Rishabh Kashyap <128898280+iamrishabhrishi@users.noreply.github.com>
2023-03-25 15:38:17 +0530

    Add files via upload

---
+++ Untitled9.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+a = int(input("Please enter the value of a  "))
+b = int(input("Please enter the value of b  "))
+c = int(input("Please enter the value of c  "))
+
+
+# In[7]:
+
+
+a,b,c
+
+
+# In[15]:
+
+
+o = (a+b)/c
+o1 = o + 50 + 5j
+
+
+# In[16]:
+
+
+str(o1) + "Rishabh"
+
+
+# In[49]:
+
+
+salary = int(input("Please enter your salary  "))
+if salary >1000:
+print("I will buy a car")
+elif salary <500: 
+print ("You better save")
+elif salary <100:
+print ("you get a better job")
+else:
+    print("Tata bye bye")
+           
+
+
+# In[50]:
+
+
+pip install autopep8
+
+
+# In[ ]:
+
+
+
+
